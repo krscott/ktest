cb15c4e543b2aa9742dc4d8940960aab689d8793
Kris Scott <kscott91@gmail.com>
2025-12-10 10:12:03 -0800

    Put KTEST_MAIN macro in smoke test

--- test/t_smoke.c
+++ test/t_smoke.c
@@ -1,15 +1,6 @@
 #include "ktest.h"
 
-static void ktest__main(struct ktest_state *ktest_state);
-
-int main(int const argc, char const *const *const argv)
-{
-    struct ktest_state state = {.opts = ktest_opts_parse(argc, argv)};
-    ktest__main(&state);
-    return ktest_end(&state);
-}
-
-static void ktest__main(struct ktest_state *ktest_state)
+KTEST_MAIN
 {
     KTEST(t_smoke)
     {
